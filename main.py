4a1fbf18f40d127a58c0d6c458034476e5073985
alvarojsanchz <alvaro.jsanchz@gmail.com>
2024-08-12 22:00:41 -0500

    Actualización con tabla de páramos

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import requests
+from collections import defaultdict
 
 app = Flask(__name__)
 
@@ -49,6 +50,39 @@ def pnn_sumapaz():
 def paramo_guargua():
     return render_weather_template('paramoGuargua.html')
 
+@app.route('/get_paramos', methods=['POST'])
+def get_paramos():
+    departamento = request.form['departamento']
+    url = f"https://www.datos.gov.co/resource/r2yz-k5z7.json?$select=nombre_paramo,sum(rea_hect_reas) as total_hectareas,nombre_de_municipio&$where=nombre_del_departamento=%22{departamento}%22&$group=nombre_paramo,nombre_de_municipio"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        
+        # Agrupar los resultados por nombre_paramo
+        resultados_agrupados = defaultdict(lambda: {'total_hectareas': 0.0, 'municipios': []})
+        
+        for item in data:
+            nombre_paramo = item['nombre_paramo']
+            nombre_municipio = item['nombre_de_municipio']
+            total_hectareas = float(item['total_hectareas']) if item['total_hectareas'] else 0.0
+            
+            resultados_agrupados[nombre_paramo]['total_hectareas'] += total_hectareas
+            resultados_agrupados[nombre_paramo]['municipios'].append(nombre_municipio)
+
+        # Convertir el resultado a una lista
+        resultados_finales = []
+        for nombre_paramo, info in resultados_agrupados.items():
+            resultados_finales.append({
+                'nombre_paramo': nombre_paramo,
+                'total_hectareas': info['total_hectareas'],
+                'municipios': ', '.join(set(info['municipios']))
+            })
+
+        return jsonify(resultados_finales)
+    else:
+        return jsonify({"error": "No se pudieron obtener los datos"}), response.status_code    
+
 def render_weather_template(template_name):
     weather_data = None
     error_message = None
@@ -63,7 +97,7 @@ def render_weather_template(template_name):
         else:
             error_message = "No se pudo obtener la información del clima."
             return render_template(template_name, error=error_message)
-    return render_template(template_name)
+    return render_template(template_name)    
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(port=5500, debug=True)
